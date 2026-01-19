b82e7158ef94a53e08f3aa56acbab6d12a452851
Sakuya020 <42985269+Sakuya020@users.noreply.github.com>
2026-01-18 20:22:42 -0500

    Update DesktopFooter.tsx

--- components/footer/DesktopFooter.tsx
+++ components/footer/DesktopFooter.tsx
@@ -7,7 +7,7 @@ const DesktopFooter = () => {
     <>
       <footer className="h-[150px] bottom-0 left-0 right-0 items-end px-[30px] py-[20px] border-t border-foreground bg-background z-30 hidden md:flex desktop-footer">
         <ul className="grid grid-cols-12 gap-[10px] w-full items-end">
-          <li className="col-span-7">(c) 2014-2025</li>
+          <li className="col-span-7">(c) 2014-2026</li>
           <Router />
         </ul>
       </footer>
